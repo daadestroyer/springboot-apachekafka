89972c09e4572741cf9e65f0dbc8ca873d93184c
Shubham Nigam <apple@Apples-MacBook-Pro.local>
2023-05-02 10:40:20 +0530

    setup done...

---
+++ src/main/java/com/daadestroyer/springbootapachekafka/SpringbootApachekafkaApplication.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.springbootapachekafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApachekafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApachekafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daadestroyer/springbootapachekafka/SpringbootApachekafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daadestroyer.springbootapachekafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApachekafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
